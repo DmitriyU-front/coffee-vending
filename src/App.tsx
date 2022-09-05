f9efca398c12a387d9ebf3c3abf02346cd1c63f9
Dmitriy U <dmitriy.ugarov@gmail.com>
2022-09-05 12:07:54 +0600

    initialization of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,109 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useContext, useState} from 'react';
 import './App.css';
+import Drink from "./components/Drink";
+import Product from "./containers/Product";
+import Wrapper from "./containers/Wrapper";
+import Container from "./containers/Background";
+import Logo from "./components/Logo";
+import Modal from "./components/Modal";
+import useModal from "./hooks/useModal";
+import OrderModal from "./components/OrderModal";
+import {drinksContext, store} from './store/context';
+
+// const drinks = [
+//     {
+//         id: 1,
+//         title: "Эспрессо",
+//         imgUrl: "/img/espresso.png",
+//         description: "Крепкий кофейный напиток, 30мл",
+//         sizes: [
+//             {size: "Обычный", price: 600},
+//             {size: "Большой>", price: 1000}
+//         ],
+//
+//         roast: [
+//             "Слабая", "Средняя", "Сильная"
+//         ],
+//         sweets: [
+//             {sweet: "Сахар", price: 50},
+//             {sweet: "Корица", price: 100},
+//             {sweet: "Сироп", price: 150},
+//         ],
+//         serving: [
+//             "В свою кружку", "В бумажный стаканчик"
+//         ]
+//     },
+//     {
+//         id: 2,
+//         title: "Американо",
+//         imgUrl: "/img/americano.png",
+//         description: "Шот эспрессо с добавлением горячей воды, 60мл"
+//     },
+//     {
+//         id: 3,
+//         title: "Капучино",
+//         imgUrl: "/img/cappuccino.png",
+//         description: "Шот эспрессо со взбитым молоком, 150мл"
+//     },
+//     {
+//         id: 4,
+//         title: "Латте",
+//         imgUrl: "/img/latte.png",
+//         description: "Шот эспрессо с двумя порциями молока, 250мл"
+//     },
+//     {
+//         id: 5,
+//         title: "Мокка",
+//         imgUrl: "/img/mocha.png",
+//         description: "Шот эспрессо с молоком и шоколадом, 200мл"
+//     },
+//     {
+//         id: 6,
+//         title: "Макиато",
+//         imgUrl: "/img/macchiato.png",
+//         description: "Двойной эспрессо с молоком и шоколадом, 250мл"
+//     },
+// ]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { isShown, toggle } = useModal();
+    const storeContext = useContext(drinksContext)
+    const [pickedDrink, setPickedDrink] = useState([])
+
+    const pickDrink = () => {
+        setPickedDrink(drink => [...pickedDrink, ])
+    }
+
+    return (
+        <drinksContext.Provider value={store}>
+            <React.Fragment>
+                 <Container>
+                     <Wrapper>
+                        <Logo/>
+                        <Product>
+                            {storeContext.map(obj => (
+                                <Drink
+                                    key={obj.id}
+                                    title={obj.title}
+                                    imgUrl={obj.imgUrl}
+                                    description={obj.description}
+                                    onClick={toggle}/>)
+                            )}
+                        </Product>
+                     </Wrapper>
+                </Container>
+                {/*<Modal isShown={isShown} hide={toggle} modalContent={*/}
+                {/*    <OrderModal*/}
+                {/*                key={obj.id}*/}
+                {/*                title={obj.title}*/}
+                {/*                imgUrl={obj.imgUrl}*/}
+                {/*                sizes={obj.sizes}*/}
+                {/*                roast={obj.roast}*/}
+                {/*                sweets={obj.sweets}*/}
+                {/*                serving={obj.serving}/>*/}
+                {/*    } />*/}
+            </React.Fragment>
+        </drinksContext.Provider>)
 }
 
 export default App;
